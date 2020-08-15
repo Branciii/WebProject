abaff0351b0a95a7431b34fbcc9015fda3acd9ff
Branka <branka.miholek.10@gmail.com>
2020-08-15 11:01:30 +0200

    trying to add picking genres for new story

--- StoryStorm/client/src/app/components/new-story/new-story.component.ts
+++ StoryStorm/client/src/app/components/new-story/new-story.component.ts
@@ -3,6 +3,7 @@ import { StoryService } from '../../services/story.service';
 import { GenreService } from '../../services/genre.service';
 import { Observable } from 'rxjs';
 import { Genre } from '../../classes/genre';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-story',
@@ -11,12 +12,18 @@ import { Genre } from '../../classes/genre';
 })
 export class NewStoryComponent implements OnInit {
 
+  storyForm: FormGroup;
+
   allGenres: Observable<Genre[]>;  
 
   constructor(private storyService: StoryService, private genreService: GenreService) { }
 
   ngOnInit() {
     this.allGenres = this.genreService.getGenres();
+    this.storyForm = new FormGroup({
+      Title: new FormControl(null, [Validators.required]),
+      Description: new FormControl(null, [Validators.required])
+    });
   }
 
 }
